4e7129447b748b5c2868685415d6b63fafbaf15d
rikusov <rikusov@gmail.com>
2021-03-10 15:37:35 +0300

    Reflection

---
+++ SharpDigDes/ForDLL/CounterWords.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ForDLL
+{
+    public class CounterWords
+    {
+
+        private Dictionary<string, int> HowWords(string s)
+        {
+            Dictionary<string, int> out_dict = new Dictionary<string, int>();
+
+
+            foreach (string item in FormatStr(s).Split(' '))
+            {
+                if (item == "" || item == " ") continue;
+                if (out_dict.ContainsKey(item)) out_dict[item]++;
+                else out_dict[item] = 1;
+            }
+
+            out_dict = out_dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            return out_dict;
+        }
+
+        private string FormatStr(string s)
+        {
+
+            string[] ElemChar = new string[] { ",", ".", "!", "?", "(", ")" };
+            string out_str = s;
+
+            foreach (string item in ElemChar) out_str = out_str.Replace(item, "");
+
+            return out_str.ToLower();
+
+        }
+
+    }
+}
---
+++ SharpDigDes/ForExe/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ForExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CW = new ForDLL.CounterWords();
+
+            var tm = CW.GetType().GetMethod("HowWords" ,BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var dict = tm.Invoke(CW,new Object[] { ReadFile() });
+
+            WriteFile((Dictionary<string,int>)dict);
+
+        }
+
+        static string ReadFile(string path = @"C:\Users\Ra19\Documents\project_hlam\Less_Sharp\Les\WAW.txt") {
+            return (new StreamReader(path)).ReadToEnd();
+        }
+
+        static void WriteFile(Dictionary<string,int> dict, string path = @"C:\Users\Ra19\Documents\project_hlam\Less_Sharp\Les\WAW.txt")
+        {
+            var SW = new StreamWriter(path);
+
+            foreach (KeyValuePair<string, int> item in dict)
+                SW.WriteLine("{0} {1}", item.Key, item.Value);
+
+            SW.Close();
+
+        }
+
+
+
+    }
+}
